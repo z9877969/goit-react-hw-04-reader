a7b8066f7895acb0ab4b425de8fb7e02869d2169
Andriy Kyrylenko <z9877969@ukr.net>
2019-11-06 15:12:39 +0200

    Updates

---
+++ precache-manifest.b36804e94e59ae4216321e0b8363faea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a7bf458230bb0844ba668350f31f74e",
+    "url": "/goit-react-hw-04-reader/index.html"
+  },
+  {
+    "revision": "2a353872e1e623f8d6ff",
+    "url": "/goit-react-hw-04-reader/static/css/main.7e88a5d2.chunk.css"
+  },
+  {
+    "revision": "0ee7969dcad60313fb0a",
+    "url": "/goit-react-hw-04-reader/static/js/2.f5c883cd.chunk.js"
+  },
+  {
+    "revision": "2a353872e1e623f8d6ff",
+    "url": "/goit-react-hw-04-reader/static/js/main.c5b2baa6.chunk.js"
+  },
+  {
+    "revision": "bb8ec70027c2c2e6c5c2",
+    "url": "/goit-react-hw-04-reader/static/js/runtime-main.c9489cfd.js"
+  }
+]);
